e85bf6fef98a7f2b57317ff8a2af1745f16b2040
Phoenix Dai <3019244328@tju.edu.cn>
2021-07-25 17:48:27 +0800

    finish the Animation

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator anim;
+    public LayerMask ground;
+    public Collider2D coll;
     public float speed = 10f;
-    
+    public float jumpForce;
     
     // Start is called before the first frame update
     void Start()
@@ -14,20 +19,51 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+     
     void FixedUpdate()
     {
         Movement();
+        SwitchAnim();
     }
 
     void Movement()
     {
-        float horizontalMove;
-        horizontalMove = Input.GetAxis("Horizontal");
+        float horizontalMove = Input.GetAxis("Horizontal");// 直接获得-1 - 1
+        float faceDirection = Input.GetAxisRaw("Horizontal");//直接获得-1，0，1
+        
+        //Player move
         if (horizontalMove != 0)
         {
-            rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
+            rb.velocity = new Vector2(horizontalMove * speed * Time.deltaTime, rb.velocity.y);
+            anim.SetFloat("running", math.abs(faceDirection));
+        }
+        if (faceDirection != 0)
+        {
+            transform.localScale = new Vector3(faceDirection, 1, 1);
+        }
+        
+        //Player jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime);
+            anim.SetBool("isJumping", true);
         }
     }
-    
+    void SwitchAnim()
+    {
+        anim.SetBool("isIdle", false);
+        if (anim.GetBool("isJumping"))
+        {
+            if (rb.velocity.y < 0)
+            {
+                anim.SetBool("isFalling", true);
+                anim.SetBool("isJumping", false);
+            }
+        }
+        else if (coll.IsTouchingLayers(ground))
+        {
+            anim.SetBool("isFalling", false);
+            anim.SetBool("isIdle", true);
+        }
+    }
 }
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 >
 >网址：https://assetstore.unity.com/packages/2d/characters/sunny-land-103349
 
-## 其他需求
+### 其他需求
 
 
 
@@ -33,5 +33,78 @@ void Movement()
             rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
         }
     }
+
+//跳跃也一样
+if (Input.GetButtonDown("Jump"))
+	{
+    rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime);
+  }
 ```
+
+#### 面向方向
+
+**区分`getAxis()`与`getAxisRaw()`**
+
+* `getAxis()`是获得[-1, 1]的连续数
+* `getAxisRaw()`是获得-1，1，0三个数
+
+
+
+所以我们需要更改player的scale来更改方向
+
+```c#
+float faceDirection = Input.GetAxisRaw("Horizontal");//直接获得-1，0，1
+if (faceDirection != 0)
+	{
+		transform.localScale = new Vector3(faceDirection, 1, 1);
+	}
+```
+
+
+
+### Animation
+
+在Sprite下创建Animator
+
+然后在Assets中创建Animator Controller；
+
+在Window中的Animation菜单中将PNG拖拽进去，然后通过更改Sample rate来更改循环速度。
+
+之后再Animator中添加parameter和transaction，来规定动画。
+
+#### 判断是否isGround
+
+1. 先获得LayerMask
+2. 添加Layers，Ground；然后将地面图层归到Ground
+3. 之后再将Script中的LayerMask定为Ground；
+4. 添加collider 变量
+5. 利用`isTouchingLayer()`来判断是否碰撞
+
+```c#
+void SwitchAnim()
+    {
+        anim.SetBool("isIdle", false);
+        if (anim.GetBool("isJumping"))
+        {
+            if (rb.velocity.y < 0)
+            {
+                anim.SetBool("isFalling", true);
+                anim.SetBool("isJumping", false);
+            }
+        }
+        else if (coll.IsTouchingLayers(ground))
+        {
+            anim.SetBool("isFalling", false);
+            anim.SetBool("isIdle", true);
+        }
+    }
+```
+
+
+
+
+
+### 镜头
+
+
 
