73422184bd6ff7419e82ebba08807c67997a52cb
Phoenix Dai <3019244328@tju.edu.cn>
2021-07-30 11:47:15 +0800

    end;

--- Assets/Script/Parallax.cs
+++ Assets/Script/Parallax.cs
@@ -18,11 +18,11 @@ public class Parallax : MonoBehaviour
     {
         if (isLockY)
         {
-            transform.position = new Vector2(startPointX + camera.position.x * moveRate, transform.position.y);
+            transform.position = new Vector3(startPointX + camera.position.x * moveRate, transform.position.y, transform.position.z);
         }
         else
         {
-            transform.position = new Vector2(startPointX + camera.position.x * moveRate, startPointY + camera.position.y * moveRate);
+            transform.position = new Vector3(startPointX + camera.position.x * moveRate, startPointY + camera.position.y * moveRate,transform.position.z);
         }
     }
 }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using Unity.Mathematics;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
